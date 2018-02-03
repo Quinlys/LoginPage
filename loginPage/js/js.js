2fd8d38cd0fd020ce9c541442e9817b82baa1b29
Igor <32520169+Quinlys@users.noreply.github.com>
2018-02-03 19:19:39 +0200

    lol

--- loginPage/js/js.js
+++ loginPage/js/js.js
@@ -26,5 +26,25 @@ $(document).ready(function(){
 });
 
 
+function getName(str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }                       
+    var filename = str.slice(i);            
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
+
+$('.click').on('click', function(e){
+ e.preventDefault();
+  $('#photo').animate({
+     width : 300,
+     height : 250
+  });
+});
+
 
 
